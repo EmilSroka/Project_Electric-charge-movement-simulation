c63ea53b3325d548d66a2e7abcdd43c2b0984a64
Emil Sroka <srokaemil@gmail.com>
2020-05-10 14:06:59 +0200

    Rename simulation to physics simulation
    Add simulation class
    Add time class

---
+++ src/dev/general/time.js
@@ -0,0 +1,18 @@
+return class Time {
+  constructor(firstTimestamp){
+    this.firstTimestamp = firstTimestamp;
+    this.currentTimestamp = firstTimestamp;
+    this.previousTimestamp = 0;
+    this.deltaTime = 0;
+  }
+
+  nextStamp(timestamp){
+    this.previousTimestamp = this.currentTimestamp;
+    this.currentTimestamp = timestamp;
+    this.deltaTime = this.currentTimestamp - this.previousTimestamp;
+  }
+
+  getDeltaTime(){
+    return this.deltaTime;
+  }
+}
---
+++ src/dev/simulation/physicsSimulation.js
@@ -0,0 +1,27 @@
+export class PhysicsSimulation {
+  constructor(entityManager){
+    this.entityManager = entityManager;
+  }
+
+  nextStep(deltaTime){
+    this.calculateElectricForces();
+    this.simulate();
+    this.handleCollisions();
+  }
+
+  calculateElectricForces(){
+    // iterate over pairs of entities and calculate acceleration
+  }
+
+  simulate(){
+    // iterate over entities and call 'simulate' methods
+  }
+
+  handleCollisions(){
+    // iterate over pairs of entities
+    // get proper collision type (collisionCheckerFactory)
+    // bounding property of entity can be: Circle | Rectangle | Array of both types
+    // if entities collide, call 'collision' method on both entities
+  }
+
+}
