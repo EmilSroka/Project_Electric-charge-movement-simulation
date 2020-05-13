03bf5cf5dd688232c2252d915d612df71eb5d37f
Emil Sroka <srokaemil@gmail.com>
2020-05-13 12:05:27 +0200

    Add drawRectangle and writeText method

--- src/dev/visualization/painter.js
+++ src/dev/visualization/painter.js
@@ -1,12 +1,30 @@
+const FONT_FAMILY = 'Arial'
+
 export class Painter{
   constructor(canvas, unitTranslator){
     this.ctx = canvas.getContext('2d');
     this.unitTranslator = unitTranslator;
   }
 
-  drawCircle(circle, fillColor, strokeColor){
+  drawRectangle(rectangle, fillColor = "white", strokeColor = "black", lineWidth = 1){
     this.ctx.fillStyle = fillColor;
     this.ctx.strokeStyle = strokeColor;
+    this.ctx.lineWidth = this.unitTranslator.toCanvasUnit(lineWidth);
+    this.ctx.beginPath();
+
+    const {x, y} = this.unitTranslator.translatePoint(rectangle.topLeftCorner)
+    const width = this.unitTranslator.toCanvasUnit(rectangle.width);
+    const height = this.unitTranslator.toCanvasUnit(rectangle.height);
+    this.ctx.rect(x, y, width, height);
+
+    this.ctx.stroke();
+    this.ctx.fill();
+  }
+
+  drawCircle(circle, fillColor = "white", strokeColor = "black", lineWidth = 1){
+    this.ctx.fillStyle = fillColor;
+    this.ctx.strokeStyle = strokeColor;
+    this.ctx.lineWidth = this.unitTranslator.toCanvasUnit(lineWidth);
     this.ctx.beginPath();
 
     const center = this.unitTranslator.translatePoint(circle.center);
@@ -15,5 +33,16 @@ export class Painter{
     
     this.ctx.stroke();
     this.ctx.fill();
+  }
+
+  writeText(bottomLeftPoint, content, fontSize, color){
+    const realFontSize = this.unitTranslator.toCanvasUnit(fontSize);
+    const position = this.unitTranslator.translatePoint(bottomLeftPoint);
+
+    this.ctx.textAlign = "center";
+    this.ctx.textBaseline = 'middle';
+    this.ctx.fillStyle = color;
+    this.ctx.font = `normal ${realFontSize}px ${FONT_FAMILY}`;
+    this.ctx.fillText(content, position.x, position.y);
   }
 }
