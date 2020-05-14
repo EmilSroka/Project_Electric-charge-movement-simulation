ea82d9d103bafea88700cb330bcf61196526735e
Hub <hubmiz20@gmail.com>
2020-05-14 10:44:13 +0200

    First version of electricCharge function to test

--- src/dev/simulation/electricCharge.js
+++ src/dev/simulation/electricCharge.js
@@ -1,3 +1,5 @@
+import { PositionVector } from "../general/geometrics";
+
 export class ElectricCharge {
   constructor(chargeType, value) {
     this.chargeType = chargeType;
@@ -13,6 +15,26 @@ export const ChargeType = {
 
 export function accelerationFromElectricity(entity1, entity2, deltaTime){
   // calculate interaction (force) between entities in given time
+  var k = 8.9875*Math.pow(10,9);
+  
+  var distance = entity1.position.distance(entity2.position);
+  var force = k*( ( Math.abs( entity1.ElectricCharge*entity2.ElectricCharge ) )/(distance**2) );
   // calculate acceleration based on force and mass of entites
+
+  var acceleration1 = force/entity1.mass;
+  var acceleration2 = force/entity2.mass;
+
+  var vector1 = new PositionVector(entity1.point,entity2.point);
+  var vector2 = new PositionVector(entity2.point,entity1.point);
+
+  vector1 = vector1.normalize();
+  vector2 = vector2.normalize();
+
+  vector1.multiply(acceleration1);
+  vector2.multiply(acceleration2);
+
+  entity1.updateAcceleration(vector1);
+  entity2.updateAcceleration(vector2);
   // return [acceleration for entity1, acceleration for entity2];
 }
+
