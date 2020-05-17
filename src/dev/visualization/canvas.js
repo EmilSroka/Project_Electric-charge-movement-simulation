48e5a2c070cb6ddeeaa526519e1f376017ceaba9
Emil Sroka <srokaemil@gmail.com>
2020-05-17 10:36:19 +0200

    Extract canvas.js from visualization.js

---
+++ src/dev/visualization/canvas.js
@@ -0,0 +1,47 @@
+import onResize from '../general/onResizeEvent';
+import { UnitTranslator } from './unitTranslator';
+import { Painter } from './painter';
+import { UIController } from './UIController';
+import { Visualization } from './visualization';
+import { States, StateFactory } from './states/stateFactory';
+import { puckFactory } from '../entities/factories/puckFactory';
+
+const WIDTH = 1920;
+const PART_OF_PAGE_WIDTH = 0.9;
+const CANVAS_RATIO = 9 / 16;
+
+export class Canvas{
+  constructor(selector, entityManager){
+    this.canvas = document.querySelector(selector);
+    this.entityManager = entityManager;
+    this.unitTranslator = new UnitTranslator(this.canvas, WIDTH, CANVAS_RATIO);
+    this.painter = new Painter(this.canvas, this.unitTranslator);
+    
+    this.UIController = new UIController(this.canvas, this.unitTranslator, this.entityManager);
+
+    this.stateFactory = new StateFactory(this.entityManager, this.UIController);
+    this.UIController.setState(this.stateFactory.get(States.AddEntityState, puckFactory(-100, -100)));
+    
+    this.updateSize();
+    this.unitTranslator.recalc();
+
+    onResize.subscribe(() => {
+      this.updateSize();
+      this.unitTranslator.recalc();
+    }); 
+
+    this.visualization = new Visualization(this.entityManager, this.painter);
+
+  }
+
+  updateSize(){
+    const width = PART_OF_PAGE_WIDTH * window.innerWidth;
+    this.canvas.width =  width;
+    this.canvas.height = CANVAS_RATIO * width;
+  }
+
+  setEntityManager(entityManager){
+    this.entityManager = entityManager;
+    this.visualization.setEntityManager(entityManager);
+  }
+}
--- src/dev/visualization/visualization.js
+++ src/dev/visualization/visualization.js
@@ -2,44 +2,36 @@ import onResize from '../general/onResizeEvent';
 import { UnitTranslator } from './unitTranslator';
 import { Painter } from './painter';
 import { Circle, Coordinates } from '../general/geometrics';
+import { LIGHT_BLUE, getColor } from '../general/colors';
+import { UIController } from './UIController';
 
 const WIDTH = 1920;
 const PART_OF_PAGE_WIDTH = 0.9;
 const CANVAS_RATIO = 9 / 16;
 
 export class Visualization {
-  constructor(selector, entityManager){
-    this.canvas = document.querySelector(selector);
+  constructor(entityManager, painter){
     this.entityManager = entityManager;
-    this.unitTranslator = new UnitTranslator(this.canvas, WIDTH, CANVAS_RATIO);
-    this.painter = new Painter(this.canvas, this.unitTranslator);
-
+    this.painter = painter;
     
-    this.updateSize();
-    this.unitTranslator.recalc();
-
-    onResize.subscribe(() => {
-      this.updateSize();
-      this.unitTranslator.recalc();
-    }); 
-
     this.nextFrame = this.nextFrame.bind(this);
     this.nextFrame();
   }
 
-  updateSize(){
-    const width = PART_OF_PAGE_WIDTH * window.innerWidth;
-    this.canvas.width =  width;
-    this.canvas.height = CANVAS_RATIO * width;
+  setEntityManager(entityManager){
+    this.entityManager = entityManager;
   }
 
   nextFrame(){
-    this.painter.drawCircle(new Circle(new Coordinates(0,0), 5000), 'white', 'white');
-
+    this.cleanCanvas();
     for(let view of this.entityManager.views()){
       view.draw(this.painter);
     }
 
     window.requestAnimationFrame(this.nextFrame);
+  }
+
+  cleanCanvas(){
+    this.painter.drawCircle(new Circle(new Coordinates(0,0), 5000), getColor(LIGHT_BLUE), getColor(LIGHT_BLUE, 0));
   }
 }
