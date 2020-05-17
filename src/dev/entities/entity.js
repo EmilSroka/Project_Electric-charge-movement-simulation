b0a91b43ec7c15fa379221632422b801db8c7e4b
Emil Sroka <srokaemil@gmail.com>
2020-05-17 10:21:51 +0200

    Add getEntityByCoordinates, isFreeArea and restore

--- src/dev/entities/entity.js
+++ src/dev/entities/entity.js
@@ -1,5 +1,6 @@
-import { ElectricCharge, ChargeType } from "../simulation/electricCharge";
 import { Coordinates, Circle } from "../general/geometrics";
+import { collisionCheckerFactory } from '../simulation/collisionDetection' 
+import globalState from '../general/state';
 
 export class Entity {
   constructor({
@@ -34,10 +35,14 @@ export class Entity {
   move(deltaTime){
     const deltaX = this.velocity.x * deltaTime + 0.5 * this.acceleration.x * Math.pow(deltaTime, 2);
     const deltaY = this.velocity.y * deltaTime + 0.5 * this.acceleration.y * Math.pow(deltaTime, 2);
+    this.translate( new Coordinates(deltaX, deltaY) );
+  }
+
+  translate(vector){
     for(const bounding of this.getArrayOfBoundings()){
-      bounding.move(deltaX, deltaY);
+      bounding.move(vector.x, vector.y);
     }
-    this.center.translate(deltaX, deltaY);
+    this.center.translate(vector.x, vector.y);
   }
 
   calculateVelocity(deltaTime){
@@ -47,4 +52,23 @@ export class Entity {
   getArrayOfBoundings(){
     return (Array.isArray(this.bounding)) ? this.bounding : [this.bounding];
   }
+
+  getMass(){
+    return globalState.getValue('massFactor') * this.mass;
+  }
 }
+
+export function collides(entity1, entity2){
+  const boundings1 = entity1.getArrayOfBoundings();
+  const boundings2 = entity2.getArrayOfBoundings();
+  for(let bounding1 of boundings1){
+    for(let bounding2 of boundings2){
+      const collisionChecker = collisionCheckerFactory(bounding1, bounding2);
+      if(collisionChecker(bounding1, bounding2)){
+        return [bounding1, bounding2];
+      }
+    }
+  }
+
+  return false;
+} 
--- src/dev/entities/entityManager.js
+++ src/dev/entities/entityManager.js
@@ -1,42 +1,92 @@
+import { deepCopy } from '../general/utils';
+import { collides, Entity } from './entity';
+import { Circle } from '../general/geometrics';
+
 export class EntityManager{
   constructor(){
     // [entity, entityView]
-    this.entities = [];
+    this.elements = [];
   }
 
   *[Symbol.iterator] () {
-    for (let k=0; k < this.entities.length; k++) {
-      yield this.entities[k][0];
+    for (let k=0; k < this.elements.length; k++) {
+      yield this.elements[k][0];
     }
   }
 
   *views(){
-    for (let k=0; k < this.entities.length; k++) {
-      yield this.entities[k][1];
+    for (let k=0; k < this.elements.length; k++) {
+      yield this.elements[k][1];
     }
   }
 
   *pairs(
     condition = () => true
   ) {
-    const entities = this.entities.filter(condition);
+    const entities = [...this].filter(condition);
     for(let i = 0; i < entities.length; i++) {
       for (let j = i + 1; j < entities.length; j++) {
-        yield [entities[i][0], entities[j][0]];
+        yield [entities[i], entities[j]];
       }
     }   
   }
 
   add(entity){
-    if(!this.entities.includes(entity)){
-      this.entities.push(entity);
+    if(!this.elements.includes(entity)){
+      this.elements.push(entity);
     }
   }
 
   remove(entity){
-    const entityIndex = this.entities.findIndex(entityArr => entityArr.electricCharge === entity.electricCharge);
+    const entityIndex = this.elements.findIndex(([entityObject]) => entityObject === entity);
     if(entityIndex != -1){
-      this.entities.splice(entityIndex, 1);
+      this.elements.splice(entityIndex, 1);
     }
+  }
+
+  deleteByCondition(condition){
+    const entityIndex = [...this].findIndex(condition);
+    if(entityIndex != -1){
+      this.elements.splice(entityIndex, 1);
+    }
+  }
+
+  takeSnapshot(){
+    const copy = [];
+    for(let [entity, view] of this.elements){
+      copy.push([deepCopy(entity), view]);
+    }
+    return copy;
+  }
+
+  restoreSnapshot(copy){
+    this.elements = copy;
+    for (let k=0; k < copy.length; k++) {
+      // this.elements[k][0] = copy[k][0];
+      // this.elements[k][1] = copy[k][1];
+      // this.elements[k][1].entity = this.elements[k][0];
+      this.elements[k][1].entity = this.elements[k][0];
+    }
+  }
+
+  isFreeArea(target){
+    for(let entity of this){
+      if(collides(target, entity) && target != entity){
+        return false;
+      }
+    } 
+    return true;
+  }
+
+  getEntityByCoordinates(coordinates){
+    const target = new Entity({
+      center: coordinates, 
+      bounding: new Circle(coordinates, 0)
+    }); //  tmp
+    for(let entity of this){
+      if(collides(target, entity)){
+        return entity;
+      }
+    } 
   }
 }
